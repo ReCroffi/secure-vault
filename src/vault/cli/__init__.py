72c339fda9bbafd69579dd2500ce2544fdfa1272
Renan Croffi <renancroffi@gmail.com>
2026-09-03 18:06:55 -0300

    feat(vault): adiciona decrypt_password e comando add da CLI

--- src/vault/cli/__init__.py
+++ src/vault/cli/__init__.py
@@ -1,6 +1,8 @@
 import typer
 
+from vault.core.crypto import encrypt_password
 from vault.core.master_password import create_vault, login
+from vault.db.credentials import save_credential
 
 app = typer.Typer()
 
@@ -29,3 +31,14 @@ def _get_key() -> bytes:
     except ValueError as e:
         typer.echo(e)
         raise typer.Exit(code=1)
+
+
+@app.command()
+def add(service_name: str, username: str):
+    password = typer.prompt(
+        "Senha do serviço", hide_input=True, confirmation_prompt=True
+    )
+    key = _get_key()
+    encrypted_password = encrypt_password(password, key)
+    save_credential(service_name, username, encrypted_password)
+    typer.echo("Adicionado com sucesso")
--- src/vault/core/crypto.py
+++ src/vault/core/crypto.py
@@ -6,3 +6,8 @@ from cryptography.fernet import Fernet
 def encrypt_password(password: str, key: bytes) -> bytes:
     fernet_key = base64.urlsafe_b64encode(key)
     return Fernet(fernet_key).encrypt(password.encode())
+
+
+def decrypt_password(encrypted_password: bytes, key: bytes) -> str:
+    fernet_key = base64.urlsafe_b64encode(key)
+    return Fernet(fernet_key).decrypt(encrypted_password).decode()
